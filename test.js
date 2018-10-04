613959c143b210c0c8c67ce7b13c952ca5da3f93
unknown <anuragpatel0055@gmail.com>
2018-10-04 19:44:42 +0530

    db data

---
+++ test.js
@@ -0,0 +1,1 @@
+[{"_id":"5bab7b80bbc92a7f1d425467","isbn":"123442123, 97885654453443","title":"Learn how to build modern web application with MEAN stack","author":"Didin J.","description":"The comprehensive step by step tutorial on how to build MEAN (MongoDB, Express.js, Angular 5 and Node.js) stack web application from scratch","published_year":"2017","publisher":"Djamware.com"}]
